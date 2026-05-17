78a41e13ab5bd84ea1265acaa35e315d85e8acd8
stevenmiller0210 <stevenmiller.prog@gmail.com>
2026-05-17 23:07:12 +0200

    Add files via upload

---
+++ kulonbozo_elemek_szama.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main() {
+    
+    int latott_szamok[100] = {0}; 
+    int szam;
+    int kulonbozo_db = 0;
+
+    printf("Adj meg 0 végjelig egész számokat az [1, 99] intervallumból!\n");
+
+   
+    while (1) {
+        printf("Szám: ");
+        scanf("%d", &szam);
+
+        
+        if (szam == 0) {
+            break; 
+        }
+
+       
+        if (szam < 1 || szam > 99) {
+            printf("Ez a szám kívül esik az elfogadható intervallumon!\n");
+        } else {
+            
+            if (latott_szamok[szam] == 0) {
+                latott_szamok[szam] = 1; 
+                kulonbozo_db++;          
+            }
+        }
+    }
+
+
+    printf("\n%d db különböző szám lett megadva.\n", kulonbozo_db);
+
+   
+    if (kulonbozo_db > 0) {
+        printf("Ezek (növekvő sorrendben): ");
+        
+        int kiirva = 0; 
+        
+        
+        for (int i = 1; i <= 99; i++) {
+            if (latott_szamok[i] == 1) {
+                printf("%d", i);
+                kiirva++;
+                
+         
+                if (kiirva < kulonbozo_db) {
+                    printf(", ");
+                }
+            }
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
